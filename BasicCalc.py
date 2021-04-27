e52c923ac63c16bf82ee59a4755e8e3a05aef809
Vik <jzenterprize90@gmail.com>
2021-04-27 13:44:53 -0400

    Simple calculator function

---
+++ BasicCalc.py
@@ -0,0 +1,20 @@
+def basic_calc():
+    num1 = float(input("Enter first number "))
+    operator = input("Enter operator ")
+    num2 = float(input("Enter second number "))
+
+    if operator == "+":
+        print(f"{num1 + num2}")
+    elif operator == "-":
+        print(f"{num1 - num2}")
+    elif operator == "*":
+        print(f"{num1 * num2}")
+    elif operator == "/":
+        print(f"{num1 / num2}")
+    elif operator == "//":
+        print(f"{num1 // num2}")
+    else:
+        print("Invalid operator")
+
+
+basic_calc()
